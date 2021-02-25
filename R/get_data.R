0a4f18bdac89eb980074888c437f2f83ea5beba6
Paul Campbell <paul.campbell@epicentre.msf.org>
2021-02-25 08:21:05 +0100

    filter continent data from JHU

--- R/get_data.R
+++ R/get_data.R
@@ -93,6 +93,7 @@ get_owid_jhcsse <- function(url = "https://raw.githubusercontent.com/owid/covid-
       region = dplyr::case_when(location == "Kosovo" ~ "Southern Europe", TRUE ~ region),
       iso_a3 = dplyr::case_when(location == "Kosovo" ~ "XKX", TRUE ~ iso_a3)
     ) %>%
+    dplyr::filter(!is.na(iso_a3)) %>% # filters out continent aggregates
     dplyr::select(date, continent, region, country = location, iso_a3, cases = new_cases, deaths = new_deaths) %>%
     dplyr::mutate_at(dplyr::vars(cases, deaths), ~ ifelse(. < 0, 0L, .))
 }
